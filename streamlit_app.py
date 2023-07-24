d498a702215d0d1985004a008cd4804fe19fe3c4
spiral023 <asi@liwest.at>
2023-07-25 01:47:32 +0200

    Update streamlit_app.py
    rot

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,3 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
-
 """
 # Welcome to Streamlit!
 
@@ -14,6 +8,12 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+
+import streamlit as st
+import altair as alt
+import pandas as pd
+import math
+from collections import namedtuple
 
 def generate_spiral_points(total_points, num_turns):
     Point = namedtuple('Point', 'x y')
@@ -34,6 +34,16 @@ def generate_spiral_points(total_points, num_turns):
 def main():
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+
+    data = generate_spiral_points(total_points, num_turns)
+
+    df = pd.DataFrame(data)
+    chart = alt.Chart(df, height=500, width=500).mark_circle(color='#FF0000', opacity=0.5).encode(x='x:Q', y='y:Q')
+
+    st.altair_chart(chart)
+
+if __name__ == "__main__":
+    main()
 
     data = generate_spiral_points(total_points, num_turns)
 
